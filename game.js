60f80cf25437eb11e2e7117503adda7068b4374f
Urdo <karol.ugarenko@gmail.com>
2022-02-25 19:56:49 +0100

    create first function

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
+
+
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    drwas: 0,
+}
+
+
+const game = {
+    playerHand: null,
+    aiHand: null,
+    playerHanHTML: null,
+}
+
+
+
+
+const hands = document.querySelectorAll(' .select img');
+
+hands.forEach(hand => hand.addEventListener('click', handSelection));
